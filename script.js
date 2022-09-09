db313b0f3dff3165fba77c9faf0b7ec0c4edf1fa
DavidS17GitHub <108815743+DavidS17GitHub@users.noreply.github.com>
2022-09-09 16:32:52 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function compute()
         return false
     }
 
-    var calculation = document.getElementById("result").innerHTML= "If you deposit <mark>"+ principal +"</mark>,\<br\>at an interest rate of <mark>" +rate+ "</mark>%\<br\>You will receive an amount of <mark>"+amount+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>" 
+    var calculation = document.getElementById("result").innerHTML= "If you deposit <mark>"+ principal +"</mark>,\<br\>at an interest rate of <mark>" +rate+ "% </mark>\<br\>You will receive an amount of <mark>"+amount+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>" 
 }
 
 function updateRate() 
